f376c05bbfc55588093afb9ed3aa2de70eab842f
Holkar <niholkar@deloitte.com>
2025-11-07 11:58:12 +0530

    added root

--- graphQL/src/main/java/com/example/graphQL/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.graphQL.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-	
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-
-}
